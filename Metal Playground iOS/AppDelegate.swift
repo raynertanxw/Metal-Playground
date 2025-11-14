d39b38ac8f14e5cd0843795c21b48c4fe144c18f
Rayner Tan <raynertanxw@me.com>
2025-11-14 17:49:51 +0800

    Removed storyboard for iOS target.

--- Metal Playground iOS/AppDelegate.swift
+++ Metal Playground iOS/AppDelegate.swift
@@ -10,12 +10,10 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- Metal Playground iOS/GameViewController.swift
+++ Metal Playground iOS/GameViewController.swift
@@ -8,46 +8,60 @@
 import UIKit
 import MetalKit
 
-// Our iOS specific view controller
 class GameViewController: UIViewController {
 
     var renderer: Renderer!
     var mtkView: MTKView!
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        self.view = UIView()
+        view.backgroundColor = .black
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        setupMTKViewAndRenderer()
+        setupFPSLabel()
+    }
+
+    private func setupMTKViewAndRenderer() {
+        guard let device = MTLCreateSystemDefaultDevice() else {
+            fatalError("Metal not supported")
+        }
+
+        let mtkView = MTKView(frame: view.bounds, device: device)
+        mtkView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mtkView.backgroundColor = .black
+
+        view.addSubview(mtkView)
+        self.mtkView = mtkView
+
+        guard let renderer = Renderer(mtkView: mtkView) else {
+            fatalError("Renderer failed")
+        }
+        self.renderer = renderer
+        mtkView.delegate = renderer
+        renderer.mtkView(mtkView, drawableSizeWillChange: mtkView.drawableSize)
+    }
+
+    private func setupFPSLabel() {
         let fpsLabel = FPSCounter(frame: CGRect(x: 20, y: 50, width: 80, height: 30))
         view.addSubview(fpsLabel)
 
-        guard let mtkView = view as? MTKView else {
-            print("View of Gameview controller is not an MTKView")
-            return
-        }
-
-        // Select the device to render with.  We choose the default device
-        guard let defaultDevice = MTLCreateSystemDefaultDevice() else {
-            print("Metal is not supported")
-            return
-        }
-        
-        mtkView.device = defaultDevice
-        mtkView.backgroundColor = UIColor.black
-
-        guard let newRenderer = Renderer(mtkView: mtkView) else {
-            print("Renderer cannot be initialized")
-            return
-        }
-        newRenderer.onFramePresented = { fps in
+        renderer.onFramePresented = { fps in
             DispatchQueue.main.async {
                 fpsLabel.updateFPS(fps)
             }
         }
-
-        renderer = newRenderer
-
-        renderer.mtkView(mtkView, drawableSizeWillChange: mtkView.drawableSize)
-
-        mtkView.delegate = renderer
     }
 }
+
